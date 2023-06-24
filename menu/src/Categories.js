69f0de571cec2ffb495f48703378279c823a6114
Osumah John <Osumah.J@bullseye.local>
2023-06-24 07:24:39 +0100

    tours and review kinda

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({categories, filterItems}) => {
+  return <div className='btn-container'>
+   {categories.map((data, index) => { return <button className='filter-btn'
+    key={index} onClick={() => filterItems(data)}>{data}</button>})}
+
+  </div>;
 };
 
 export default Categories;
